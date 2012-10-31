26c575a37969645f9786b17f0db430810aa210bc
Matt Hughes <hughesm02@gmail.com>
2012-10-31 15:10:04 -0400

    started the empty file

---
+++ backbone-tables.js
@@ -0,0 +1,18 @@
+/**
+ * Backbone-tables.js 0.0.0
+ * by Matt Hughes
+ */
+(function () {
+    "use strict";
+    var _, Backbone;
+    _ = window._;
+    Backbone = window.Backbone;
+    Backbone.Table = {
+        model: Backbone.Model.extend({
+
+        }),
+        view: Backbone.View.extend({
+
+        })
+    };
+}());
